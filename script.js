1a51df22f4ff01374d8e65a2407b25003295eba4
Connie <connieyzhu3@gmail.com>
2023-07-21 09:47:00 -0700

    Create 16x16 div grid with flexbox

---
+++ script.js
@@ -0,0 +1,37 @@
+const grid = document.getElementById('grid');
+
+function createGrid(num){
+    for(r = 0; r < num; r++){
+        const row = document.createElement('div');
+        row.className = 'row';
+        for(c = 0; c < num; c++){
+            const square = document.createElement('div');
+            square.className = 'square';
+            square.style.width = squareSize(num);
+            square.style.height = squareSize(num);
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    }
+}
+
+function defaultGrid(){
+    createGrid(16);
+}
+
+function squareSize(num){
+    const size = 500 / num;
+    return size;
+}
+
+function changeGridSize(size){
+
+}
+
+function colorGrid(){
+
+}
+
+defaultGrid();
+
+console.log(document.querySelectorAll('div'));
